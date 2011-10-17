f2397bb4f95b62d675143e53e169850f892d9b4f
Santhosh Kumar <santho89@gmail.com>
2011-10-17 21:26:53 +0530

    completed the initial design for the main_page

--- apps/sproutdemo/resources/main_page.js
+++ apps/sproutdemo/resources/main_page.js
@@ -11,14 +11,76 @@ Sproutdemo.mainPage = SC.Page.design({
   // Add childViews to this pane for views to display immediately on page 
   // load.
   mainPane: SC.MainPane.design({
-    childViews: 'labelView'.w(),
-    
-    labelView: SC.LabelView.design({
-      layout: { centerX: 0, centerY: 0, width: 200, height: 18 },
-      textAlign: SC.ALIGN_CENTER,
-      tagName: "h1", 
-      value: "Welcome to SproutCore!"
-    })
-  })
+    childViews: 'toolbarView splitView bottomToolbar'.w(),
+      toolbarView: SC.ToolbarView.design({
+        layout: {top: 0, left: 0, right: 0, height: 100},
+        backgroundColor: 'black',
+        classNames: ['top-toolbar'],
+        childViews: 'select home profile logout'.w(),
+        
+        select: SC.SelectFieldView.design({
+          layout: {right:20, top: 20, height: 20, width: 100},
+          objects: ['English', 'German', 'French']
+        }),
 
+        home: SC.ButtonView.design({
+          layout: {right: 180, bottom: 10, height: 25, width: 75},
+		  classNames:['toolbar-button'],
+          title: 'Home'
+        }),
+
+        profile: SC.ButtonView.design({
+          layout: {right: 105, bottom: 10, height: 25, width: 75},
+		  classNames:['toolbar-button'],
+          title: 'Profile'
+        }),
+
+        logout: SC.ButtonView.design({
+          layout: {right: 30, bottom: 10, height: 25, width: 75},
+          classNames:['toolbar-button'],
+		  title: 'Logout'
+        })
+      }),
+
+      splitView: SC.SplitView.design({
+        layout: {top:100, left: 0, right: 0, bottom: 50},
+        topLeftMinThickness: 150,
+        topLeftMaxThickness: 150,
+		bottomRightMinThickness: 450,
+        defaultThickness: 150,
+        canCollapseViews: NO,
+        topLeftView: SC.View.design({
+          backgroundColor: 'gray',
+          childViews: 'list'.w(),
+
+          list: SC.ListView.design({
+            layout: {top: 0, left: 0, right: 0, height: 125},
+            rowHeight: 25,
+			contentValueKey: 'name',
+            content: [
+				SC.Object.create({name: 'Profile', value: 'profile'}), SC.Object.create({name: 'Company Profile', value: 'company_profile'}), SC.Object.create({name: 'User Profile', value: 'user_profile'}), SC.Object.create({name: 'Subscriptions', value: 'subscriptions'}), SC.Object.create({name: 'Log', value: 'log'})]
+          })
+        }),
+
+        bottomRightView: SC.View.design({
+          backgroundColor: 'white',
+          
+        })
+      }),
+	  bottomToolbar: SC.ToolbarView.design({
+        layout: {bottom: 0, left: 0, right: 0, height: 50},
+        backgroundColor: 'black',
+        classNames: ['top-toolbar'],
+		childViews: 'copyright'.w(),
+		
+		copyright: SC.LabelView.design({
+			layout: {left: 20, width: 140, centerY: 0, height: 25},
+			classNames: ['copyright'],
+			value: 'icloud Feedback @ 2011'
+		})
+	  })
+    })
 });
+
+
+
---
+++ tmp/debug/build/static/sproutdemo/en/current/source/resources/main_page.js
@@ -0,0 +1,87 @@
+// ==========================================================================
+// Project:   Sproutdemo - mainPage
+// Copyright: @2011 My Company, Inc.
+// ==========================================================================
+/*globals Sproutdemo */
+
+// This page describes the main user interface for your application.  
+Sproutdemo.mainPage = SC.Page.design({
+
+  // The main pane is made visible on screen as soon as your app is loaded.
+  // Add childViews to this pane for views to display immediately on page 
+  // load.
+  mainPane: SC.MainPane.design({
+    childViews: 'toolbarView splitView bottomToolbar'.w(),
+      toolbarView: SC.ToolbarView.design({
+        layout: {top: 0, left: 0, right: 0, height: 100},
+        backgroundColor: 'black',
+        classNames: ['top-toolbar'],
+        childViews: 'select home profile logout'.w(),
+        
+        select: SC.SelectFieldView.design({
+          layout: {right:20, top: 20, height: 20, width: 100},
+          objects: ['English', 'German', 'French']
+        }),
+
+        home: SC.ButtonView.design({
+          layout: {right: 180, bottom: 10, height: 25, width: 75},
+		  classNames:['toolbar-button'],
+          title: 'Home'
+        }),
+
+        profile: SC.ButtonView.design({
+          layout: {right: 105, bottom: 10, height: 25, width: 75},
+		  classNames:['toolbar-button'],
+          title: 'Profile'
+        }),
+
+        logout: SC.ButtonView.design({
+          layout: {right: 30, bottom: 10, height: 25, width: 75},
+          classNames:['toolbar-button'],
+		  title: 'Logout'
+        })
+      }),
+
+      splitView: SC.SplitView.design({
+        layout: {top:100, left: 0, right: 0, bottom: 50},
+        topLeftMinThickness: 150,
+        topLeftMaxThickness: 150,
+		bottomRightMinThickness: 450,
+        defaultThickness: 150,
+        canCollapseViews: NO,
+        topLeftView: SC.View.design({
+          backgroundColor: 'gray',
+          childViews: 'list'.w(),
+
+          list: SC.ListView.design({
+            layout: {top: 0, left: 0, right: 0, height: 125},
+            rowHeight: 25,
+			contentValueKey: 'name',
+            content: [
+				SC.Object.create({name: 'Profile', value: 'profile'}), SC.Object.create({name: 'Company Profile', value: 'company_profile'}), SC.Object.create({name: 'User Profile', value: 'user_profile'}), SC.Object.create({name: 'Subscriptions', value: 'subscriptions'}), SC.Object.create({name: 'Log', value: 'log'})]
+          })
+        }),
+
+        bottomRightView: SC.View.design({
+          backgroundColor: 'white',
+          
+        })
+      }),
+	  bottomToolbar: SC.ToolbarView.design({
+        layout: {bottom: 0, left: 0, right: 0, height: 50},
+        backgroundColor: 'black',
+        classNames: ['top-toolbar'],
+		childViews: 'copyright'.w(),
+		
+		copyright: SC.LabelView.design({
+			layout: {left: 20, width: 140, centerY: 0, height: 25},
+			classNames: ['copyright'],
+			value: 'icloud Feedback @ 2011'
+		})
+	  })
+    })
+});
+
+
+
+; if ((typeof SC !== 'undefined') && SC && SC.Module && SC.Module.scriptDidLoad) SC.Module.scriptDidLoad('sproutdemo');
